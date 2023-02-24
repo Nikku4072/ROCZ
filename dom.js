7c6b9771d175943c2f062e8bcc23a0d55d15d103
nikku <nikku4072192@gmail.com>
2023-02-24 14:48:13 +0530

    hi

--- dom.js
+++ dom.js
@@ -19,7 +19,16 @@ items[2].style.fontWeight='bold';
 items[3].style.fontWeight='bold';
 items[2].style.backgroundColor='green';
 
+var secondItem=document.querySelector('.list-group-item:nth-Child(2)');
+secondItem.style.color='green';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='green';
+    even[i].style.backgroundColor='blue';
 
+}
 
 
 
