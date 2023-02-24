415cfeb7cb1df7d6f099a1ef454b8c71f9e9f20a
nikku <nikku4072192@gmail.com>
2023-02-24 15:28:17 +0530

    nice

--- dom.js
+++ dom.js
@@ -1,34 +1,30 @@
-//examine the document object//
-console.dir(document);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.head);
-console.log(document.all[10]);
-var headerTitle = document.getElementById('header-title');
-headerTitle.innerHTML = '<h3>hello</h3>';
-var header=document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000';
-//get elemnts by class name
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='hello 2';
-items[0].style.fontWeight='bold';
-items[1].style.fontWeight='bold';
-items[2].style.fontWeight='bold';
-items[3].style.fontWeight='bold';
-items[2].style.backgroundColor='green';
 
-var secondItem=document.querySelector('.list-group-item:nth-Child(2)');
-secondItem.style.color='green';
-var odd=document.querySelectorAll('li:nth-child(odd)');
-var even=document.querySelectorAll('li:nth-child(even)');
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor='green';
-    even[i].style.backgroundColor='blue';
 
-}
+//travesing rhe dom
+var itemList=document.querySelector('#items');
+//parentnode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor='pink';
+console.log(itemList.parentNode.parentNode);
+//parent element
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='pink';
+console.log(itemList.parentElement.parentElement);
+//child nodes
+console.log(itemList.childNodes);
+
+
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor='yellow';
+//firstchild
+console.log(itemList.firstChild);
+itemList.firstElementChild.textContent='hello';
+
+
+
+
+
 
 
 
